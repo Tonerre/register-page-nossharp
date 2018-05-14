ced22c41562ef049fca6585ac0729000eaa81ced
Tonerre <boukottaya.jr@gmail.com>
2018-05-14 21:50:31 +0200

    split server file into server + database

---
+++ database.js
@@ -0,0 +1,53 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('postgres', 'postgres', 'root', {
+    host: 'localhost',
+    dialect: 'postgres',
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+const accounts = sequelize.define('accounts', {
+    username: {
+        type: Sequelize.STRING,
+        primaryKey: true
+    },
+    email: {
+        type: Sequelize.STRING,
+        primaryKey: true
+    },
+    password: {
+        type: Sequelize.STRING
+    },
+    registrationIP: {
+        type: Sequelize.STRING
+    },
+    emailRegistration: {
+        type: Sequelize.STRING
+    },
+    authority: {
+        type: Sequelize.INTEGER
+    }
+});
+
+
+
+module.exports = {
+    Sequelize,
+    sequelize,
+    accounts
+} 
--- server.js
+++ server.js
@@ -1,53 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const Sequelize = require('sequelize');
 const reqip = require('request-ip');
 const sha512 = require('sha512');
+const db = require('./database.js');
+
 app.use(express.static(__dirname + '/public'))
-
-const sequelize = new Sequelize('postgres', 'postgres', 'root', {
-    host: 'localhost',
-    dialect: 'postgres',
-
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-});
-
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
-
-
-const accounts = sequelize.define('accounts', {
-    Username: {
-        type: Sequelize.STRING
-    },
-    Email: {
-        type: Sequelize.STRING
-    },
-    Password: {
-        type: Sequelize.STRING
-    },
-    RegistrationIP: {
-        type: Sequelize.STRING
-    },
-    EmailRegistration:{
-        type: Sequelize.STRING
-    },
-    Authority:{
-        type: Sequelize.INTEGER
-    }
-});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -62,22 +20,20 @@ app.get('/', (req, res) => {
 app.post('/register', (req, res) => {
     const clientIp = reqip.getClientIp(req);
     
-    if (req.body.Password !== req.body.repeatPassword) {
-        return res.send("Password not equals");
-    }
-
-    sequelize.sync({ force: false })
-        .then(() => accounts.create({
-            Username: req.body.Username,
-            Email: req.body.Email,
-            Password: sha512(req.body.Password).toString('hex'),
-            RegistrationIP: clientIp,
-            EmailRegistration: req.body.Email,
-            Authority: 0
+    db.sequelize.sync({ force: false })
+        .then(() => db.accounts.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: sha512(req.body.password).toString('hex'),
+            registrationIP: clientIp,
+            emailRegistration: req.body.email,
+            authority: 0
         }))
         .then(e => {
             console.log(e.toJSON());
         });
+
+   
 
     res.redirect('/success');
 });
